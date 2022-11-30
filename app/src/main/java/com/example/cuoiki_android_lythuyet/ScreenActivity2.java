bb5e9df8f1de6d30c2cc40b861e16e71b9345dd6
kngansokewt <tranngan55599@gmail.com>
2022-11-30 16:14:33 +0700

    feature-TranThiKimNgan-UI_Started_SignUp_SignIn_ForgetPass

---
+++ app/src/main/java/com/example/cuoiki_android_lythuyet/ScreenActivity2.java
@@ -0,0 +1,15 @@
+package com.example.cuoiki_android_lythuyet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ScreenActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen2);
+    }
+}
+
